40cbd707f9b32a2c908f018e48b464892719d7af
Mark A. Ropper <mark@chavfreezone.me.uk>
2015-08-03 15:37:13 +0100

    Popup panels from chrome extension added, placeholder icons added, bootstrap and jquery added. index.js updated to add button and show/hide panel

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-var self = require('sdk/self');
+var { ToggleButton } = require("sdk/ui/button/toggle");
+var panels = require("sdk/panel");
+var self = require("sdk/self");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
+var button = ToggleButton({
+    id: "button",
+    label: "Panic Button",
+    icon: {
+        "16": "./icon-16.png",
+        "32": "./icon-32.png",
+        "64": "./icon-64.png"
+    },
+    onChange: handleChange
+});
+
+var panel = panels.Panel({
+    contentURL: self.data.url("popup.html"),
+    onHide: handleHide
+});
+
+function handleChange(state)
+{
+    if(state.checked)
+    {
+        panel.show({
+            position: button
+        });
+    }
 }
 
-exports.dummy = dummy;
+function handleHide()
+{
+    button.state("window", {checked: false});
+}
