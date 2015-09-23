88a33aa98a7ed370969a6b81fcd19a4b1f6259bd
Mark A. Ropper <mark@chavfreezone.me.uk>
2015-09-23 12:31:36 +0100

    Updated icons and images, revised versioning, modified panel to fit with new images

--- index.js
+++ index.js
@@ -9,16 +9,19 @@ var button = ToggleButton({
     id: "button",
     label: "Panic Button",
     icon: {
-        "16": "./icon-16.png",
-        "32": "./icon-32.png",
-        "64": "./icon-64.png"
+        "16": "./icon16.png",
+        "19": "./icon19.png",
+        "32": "./icon32.png",
+        "48": "./icon48.png",
+        "64": "./icon64.png",
+        "128": "./icon128.png"
     },
     onChange: handleChange
 });
 
 var panel = panels.Panel({
     width: 210,
-    height: 274,
+    height: 275,
     contentURL: self.data.url("popup.html"),
     contentScriptFile: ["./jquery.min.js", "./popup.js"],
     onHide: handleHide
