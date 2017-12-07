648abc029a4c2b263f332898fd113920a0487d74
Dawn\Mark <mark@chavfreezone.me.uk>
2017-12-07 18:39:39 +0000

    Removed dependency on jquery, converted 4-space tabs to 2-space tabs

--- data/nsfwguard.js
+++ data/nsfwguard.js
@@ -20,58 +20,58 @@ browser.storage.sync.get({nsfwGuardEnabled: true, safehavenUrl: "https://www.red
 
 panicbutton.processUrl = function()
 {
-    panicbutton.url = window.location.href;
-    // Check for NSFW Subreddit
-    var regex = /^https?:\/\/[^\.]*\.?reddit.com\/r\/[^\/]+\/?/
-    let regexResult = regex.exec(panicbutton.url);
-    if(regexResult != null) 
-    {
-      panicbutton.url = regexResult[0];
-    }
-    else // Check for an over18 page
-    {
-      let regex = /^https?:\/\/[^\.]*\.?reddit.com\/over18\?.*/
-      let regexResult = regex.exec(panicbutton.url);
-      if(regexResult != null)
-      {
-        // Caught an over18 access page, redirect to the safe haven
-        window.location.replace(panicbutton.safehavenUrl);
-      }
-    }
+  panicbutton.url = window.location.href;
+  // Check for NSFW Subreddit
+  var regex = /^https?:\/\/[^\.]*\.?reddit.com\/r\/[^\/]+\/?/
+  let regexResult = regex.exec(panicbutton.url);
+  if(regexResult != null) 
+  {
     panicbutton.url = regexResult[0];
-
     // Check if we need to add a '/' to the end
     if(panicbutton.url.slice(-1) != "/")
     {
-        panicbutton.url +=  "/";
+      panicbutton.url +=  "/";
     }
 
     // Append the about.json we want to check
     panicbutton.jsonUrl = panicbutton.url + "about.json";
+  }
+  else // Check for an over18 page
+  {
+    let regex = /^https?:\/\/[^\.]*\.?reddit.com\/over18\?.*/
+    let regexResult = regex.exec(panicbutton.url);
+    if(regexResult != null)
+    {
+      // Caught an over18 access page, redirect to the safe haven
+      window.location.replace(panicbutton.safehavenUrl);
+    }
+  }  
 }
 
 panicbutton.checkForNSFW = function()
 {
-    panicbutton.processUrl();
-    $.get(panicbutton.jsonUrl, function(json)
+  panicbutton.processUrl();
+  fetch(panicbutton.jsonUrl, {method: 'GET'})
+  .then((response) => { return response.json();})
+  .then((json) => {
+    if(typeof json != "undefined")
     {
-        if(typeof json.data != "undefined")
-        {
-            if(json.data.over18)
-            {
-                window.location.replace(panicbutton.safehavenUrl);
-            }
-        }
-        else if(typeof json[0].data.children[0] != "undefined") // Might be a comments page
-        {
-            if(json[0].data.children[0].data.over_18)
-            {
-                window.location.replace(panicbutton.safehavenUrl);
-            }
-        }
-        else
-        {
-            console.log("NSFW Guard Is Confused! Please contact app@nofap.com");
-        }
-    });
+      if(json.data.over18)
+      {
+        window.location.replace(panicbutton.safehavenUrl);          
+      }
+    }
+    else if(typeof json[0].data.children[0] != "undefined") // Might be a comments page
+    {
+      if(json[0].data.children[0].data.over_18)
+      {
+        window.location.replace(panicbutton.safehavenUrl);          
+      }
+    }
+    else
+    {
+      console.log("NSFW Guard Is Confused! Please contact app@nofap.com");        
+    }
+  })
+  .catch((err) => { console.error(err); })
 }
